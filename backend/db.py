a7b5d5a45b906557a675178dfa53817fa47ff2b8
Jayanga <jayangae5@gmail.com>
2026-07-01 11:40:15 +0530

    Configure MongoDB using environment variables for deployment

--- backend/db.py
+++ backend/db.py
@@ -1,19 +1,16 @@
 import os
-
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://glowmatchadmin:GlowAI2026@cluster0.eavwsh7.mongodb.net/?appName=Cluster0")
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
 
 db = client["glowmatch_ai"]
 
 analysis_collection = db["analysis"]
-
 users_collection = db["users"]
-
 chat_collection = db["chat_history"]
-
 favorites_collection = db["favorites"]
-
 admins_collection = db["admins"]
 
 print("MongoDB Connected Successfully")
