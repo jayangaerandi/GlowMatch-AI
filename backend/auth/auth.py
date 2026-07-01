c7dcb4237f9e8697d09b1780b263bc3e4c72ad7b
Jayanga <jayangae5@gmail.com>
2026-07-01 23:13:30 +0530

    Fix change password

--- backend/auth/auth.py
+++ backend/auth/auth.py
@@ -68,15 +68,29 @@ def login_user(email, password):
 
 def change_password(email, current_password, new_password):
 
+    print("EMAIL:", email)
+    print("CURRENT PASSWORD:", current_password)
+    print("NEW PASSWORD:", new_password)
+
     user = users_collection.find_one({
         "email": email
     })
+
+    print("USER FOUND:", user)
 
     if not user:
         return {
             "success": False,
             "message": "User not found"
         }
+
+    print(
+        "PASSWORD MATCH:",
+        bcrypt.checkpw(
+            current_password.encode(),
+            user["password"].encode()
+        )
+    )
 
     if not bcrypt.checkpw(
         current_password.encode(),
@@ -93,7 +107,7 @@ def change_password(email, current_password, new_password):
         bcrypt.gensalt()
     ).decode()
 
-    users_collection.update_one(
+    result = users_collection.update_one(
         {"email": email},
         {
             "$set": {
@@ -102,7 +116,9 @@ def change_password(email, current_password, new_password):
         }
     )
 
+    print("MODIFIED COUNT:", result.modified_count)
+
     return {
         "success": True,
         "message": "Password changed successfully"
-    }    
+    }
